46e3d853695da00179a7043d689db02bf7a7aeb2
Josip <dalmat.development@gmail.com>
2017-07-11 20:14:30 +0200

    improve nav

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,11 +1,20 @@
 import $ from 'jquery';
 import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
 	constructor() {
 		this.nav = $('nav');
 		this.navTrigger = $(".section-features");
 		this.createNavWaypoint();
+		this.pageSections = $(".section");
+		this.headerLinks = $(".main-navigation a");
+		this.createPageSectionWaypoints();	
+		this.addSmoothScroll();
+	}
+
+	addSmoothScroll() {
+		this.headerLinks.smoothScroll();
 	}
 
 	createNavWaypoint() {
@@ -20,7 +29,37 @@ class StickyHeader {
 				}
 			},
 
-			offset: "50%"
+			offset: "87%"
+		});
+	}
+
+	createPageSectionWaypoints() {
+		var that = this;
+		this.pageSections.each(function() {
+			var currentPageSection = this;
+			new Waypoint({
+				element: currentPageSection,
+				handler: function(direction) {
+					if(direction === "down") {
+						var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+						that.headerLinks.removeClass("is-current-link");
+						$(matchingHeaderLink).addClass("is-current-link");
+					}
+				},
+				offset: "18%"
+			});
+
+			new Waypoint({
+				element: currentPageSection,
+				handler: function(direction) {
+					if(direction === "up") {
+						var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+						that.headerLinks.removeClass("is-current-link");
+						$(matchingHeaderLink).addClass("is-current-link");
+					}
+				},
+				offset: "-40%"
+			});
 		});
 	}
 }
